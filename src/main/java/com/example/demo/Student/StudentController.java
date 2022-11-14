d4ff9c715021cfe07e93b1c29a63b961a2f72fa7
Hpkarimi <hpkarimi110@gmail.com>
2022-11-14 17:12:25 +0330

    the end!

--- src/main/java/com/example/demo/Student/StudentController.java
+++ src/main/java/com/example/demo/Student/StudentController.java
@@ -1,11 +1,13 @@
 package com.example.demo.Student;
 
+import org.aspectj.apache.bcel.classfile.Module;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 @RestController
-@RequestMapping(path = "api")
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
     private final StudentService studentService;
     @Autowired
@@ -20,6 +22,14 @@ public class StudentController {
     @PostMapping
     public void newStudent(@RequestBody Student student){
         StudentService.addNewStudent(student);
+    }
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudentById(studentId);
+    }
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId, @RequestParam(required =false) String name,@RequestParam(required = false) String email){
+        studentService.updateStudentById(studentId,name,email);
     }
 
 }
--- src/main/java/com/example/demo/Student/StudentService.java
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -1,16 +1,16 @@
 package com.example.demo.Student;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDate;
-import java.time.Month;
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Component
 // or we can use @Service instead it.
 public class StudentService {
-    private final StudentRepository studentRepository;
+    private static StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
@@ -22,6 +22,38 @@ public class StudentService {
         return studentRepository.findAll();
     }
     public static void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+//        if (studentOptional.isPresent()){
+//            throw new IllegalStateException("email taken!");
+//
+//        }
+        studentRepository.save(student);
         System.out.println(student);
+    }
+
+
+    public void deleteStudentById(Long studentId) {
+        boolean answer = studentRepository.existsById(studentId);
+        if (!answer) {
+            throw new IllegalStateException("this id does not exist!");
+        }
+        studentRepository.deleteById(studentId);
+    }
+    @Transactional
+    public void updateStudentById(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(()-> new IllegalStateException(
+                        "student with id "+studentId+" does not exist!"
+                ));
+        if (name != null && name.length()>0 && !Objects.equals(student.getName(),name)) {
+            student.setName(name);
+        }
+        if (email != null && email.length()>0&&Objects.equals(student.getEmail(),email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
